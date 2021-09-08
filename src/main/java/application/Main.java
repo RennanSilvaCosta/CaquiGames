3596f1eac1724ddb1d7215a54909ba6b3e7c4cb0
Rennan Costa <rennansilvacosta@hotmail.com>
2021-09-08 20:59:34 -0300

    testando conexão com o db e inserindo um registro

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -7,17 +7,31 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("caqui");
+
+        Usuario user = new Usuario();
+        user.setNome("Rennan Costa");
+        user.setEmail("rennan@hotmail.com");
+
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+
         Parent root = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
         Scene scene = new Scene(root);
-        scene.setFill(Color.TRANSPARENT);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
         primaryStage.show();
         scene.getRoot().requestFocus();
 
