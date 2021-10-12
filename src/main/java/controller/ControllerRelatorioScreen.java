212b69030efdd4649aa4cbc8abc2cf82cd43ad9c
Rennan Costa <rennansilvacosta@hotmail.com>
2021-10-12 10:24:39 -0300

    criando tela de relatorio

--- src/main/java/controller/ControllerRelatorioScreen.java
+++ src/main/java/controller/ControllerRelatorioScreen.java
@@ -1,4 +1,33 @@
 package controller;
 
-public class ControllerRelatorioScreen {
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerRelatorioScreen implements Initializable {
+
+    @FXML
+    JFXButton btnRelatorioSintetico, btnRelatorioAnalitico, btnSair;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+        btnRelatorioSintetico.setGraphic(new ImageView(new Image("/icons/btn_relatorio_sintetico.png")));
+        btnRelatorioAnalitico.setGraphic(new ImageView(new Image("/icons/btn_relatorio_analitico.png")));
+
+    }
+
+    public void fecharJanela() {
+        Stage stage = (Stage) btnSair.getScene().getWindow();
+        stage.close();
+    }
 }
