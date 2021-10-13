60b0fe46e3261e975b6ff3c1500a8821b0f6af79
Rennan Costa <rennansilvacosta@hotmail.com>
2021-10-13 11:10:21 -0300

    criando tela de login

--- src/main/java/controller/ControllerLoginScreen.java
+++ src/main/java/controller/ControllerLoginScreen.java
@@ -1,4 +1,26 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
 public class ControllerLoginScreen {
+
+    @FXML
+    JFXButton btnSair, btnLogar;
+
+    @FXML
+    JFXTextField txtEmail;
+
+    @FXML
+    JFXPasswordField txtSenha;
+
+
+    public void fecharJanela() {
+        Stage stage = (Stage) btnSair.getScene().getWindow();
+        stage.close();
+    }
+
 }
