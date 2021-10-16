69722f33dcf08f98c8990ff2aec6a6a3168f90e9
edsoncosta <edsonlimajll@gmail.com>
2021-10-15 23:26:06 -0300

    Criando ClienteDAO e metodo criaCliente

---
+++ src/main/java/dao/ClienteDAO.java
@@ -0,0 +1,35 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
+
+public class ClienteDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void criaCliente(String nome, String cpf, String celular, String email, LocalDate dataNasc ) {
+
+        final StringBuilder createCliente = new StringBuilder();
+
+        createCliente.append("insert into Cliente ");
+        createCliente.append("(nome, ");
+        createCliente.append("cpf, ");
+        createCliente.append("celular, ");
+        createCliente.append("email, ");
+        createCliente.append("dataNasc, ");
+        createCliente.append("endereco) ");
+        createCliente.append("values ");
+        createCliente.append("( " + nome);
+        createCliente.append("( " + cpf);
+        createCliente.append("( " + celular);
+        createCliente.append("( " + email);
+        createCliente.append("( " + dataNasc);
+        createCliente.append(")");
+
+        this.em.createNativeQuery(createCliente.toString()).executeUpdate();
+
+    }
+
+}
