86127214bd356edbf2a826612720cd2ff3bc8143
edsoncosta <edsonlimajll@gmail.com>
2021-10-20 00:18:45 -0300

    Incluindo ProdutoService

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -7,10 +7,30 @@ import java.util.List;
 
 public class ProdutoService {
 
-    private ProdutoDAO pr = new ProdutoDAO();
+    private ProdutoDAO produtoDAO = new ProdutoDAO();
 
-    public List<Produto> listarNomeProdutos() {
-        return pr.listarNomesProdutos();
+    public void cadastraProduto(Produto produto) {
+        produtoDAO.cadastraProduto(produto);
+    }
+
+    public List<Produto> buscaListaProdutos(String str) {
+        return produtoDAO.getProdutosPorNome(str);
+    }
+
+    public List<Produto> buscaListaProdutos() {
+        return produtoDAO.listarNomesProdutos();
+    }
+
+    public List<Produto> buscaProdutosPelaMarca(String str) {
+        return produtoDAO.getProdutosPorMarca(str);
+    }
+
+    public void atualizaProduto(Produto produto) {
+        produtoDAO.editaProduto(produto);
+    }
+
+    public void excluiProduto(String descricao) {
+        produtoDAO.deletaProduto(descricao);
     }
 
 }
