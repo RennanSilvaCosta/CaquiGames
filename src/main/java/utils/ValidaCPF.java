28af17bc0b45f547d86238d103c6fab226251c25
edsoncosta <edsonlimajll@gmail.com>
2021-10-27 11:14:07 -0300

    Ajuste na classe ValidaCPF

--- src/main/java/utils/ValidaCPF.java
+++ src/main/java/utils/ValidaCPF.java
@@ -11,36 +11,11 @@ import java.util.List;
 
 public class ValidaCPF {
 
-    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "caqui" );
-    private EntityManager entityManager = entityManagerFactory.createEntityManager();
-
     public boolean isCPFValido( String cpf ) {
         CPFValidator cpfValidator = new CPFValidator();
         List< ValidationMessage > erros = cpfValidator.invalidMessagesFor( cpf );
-        if( erros.size() > 0 ) {
-            return false;
-        } else {
-            return true;
-        }
-
+        return erros.isEmpty();
     }
-
-    public Funcionario buscaFuncionarioPorCPF( String cpf ) throws NoResultException {
-        String getFuncionarioPorCPF = "SELECT f FROM Funcionario f WHERE cpf = :cpf";
-        TypedQuery< Funcionario > typedQuery = entityManager
-                .createQuery( getFuncionarioPorCPF, Funcionario.class )
-                .setParameter( "cpf", cpf );
-        return typedQuery.getSingleResult();
-    }
-
-    public Cliente buscaClientePorCPF( String cpf ) throws NoResultException {
-        String getClientePorCPF = "SELECT c FROM Cliente c WHERE cpf = :cpf";
-        TypedQuery< Cliente > typedQuery = entityManager
-                .createQuery( getClientePorCPF, Cliente.class )
-                .setParameter( "cpf", cpf );
-        return typedQuery.getSingleResult();
-    }
-
 }
 
 
